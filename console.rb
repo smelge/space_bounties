eaeae77a937d27c13d13d6bd502d3507c8dee22d
smelge <traildogfilms@gmail.com>
2018-09-25 21:11:38 +0100

    Giving up for tonight. Updates failed.

--- console.rb
+++ console.rb
@@ -2,22 +2,124 @@ require_relative('models/space_bounties.rb')
 require('pry')
 Bounties.delete_all()
 
-bounty1 = Bounty.new({
+bounty1 = Bounties.new({
   'name' => 'Zarflop Granular',
   'species' => 'Splarj',
   'bounty' => 70000
 })
 
-bounty2 = Bounty.new({
+bounty2 = Bounties.new({
   'name' => 'Beeflord Jamstar',
   'species' => 'Giant Sentient Carrot',
   'bounty' => 120000
 })
 
-bounty3 = Bounty.new({
+bounty3 = Bounties.new({
   'name' => 'Percival Schwoop',
   'species' => 'Slartian',
   'bounty' => 1400
 })
 
-bounty1.add_bounty()
+bounty1.save()
+bounty2.save()
+bounty3.save()
+
+quitloop = false
+
+until quitloop == true
+  p "=================================="
+  p "Select an Option"
+  p "=================================="
+  p "1 - View all Bounties"
+  p "2 - Add a Bounty"
+  p "3 - Update a Bounty"
+  p "4 - Delete a Bounty"
+  p "5 - Quit"
+  p "=================================="
+  p "Make a selection: "
+  input = gets.chomp.to_i
+
+  case input
+    when 1
+      p "=================================="
+      p "View All Bounties"
+      p "=================================="
+      bounty_list = Bounties.all()
+      bounty_list.each do |bounty|
+        p "Bounty #{bounty.id}: #{bounty.name} (#{bounty.species}) - Ω#{bounty.bounty}"
+      end
+      if bounty_list.count == 0
+        p "No Records"
+      end
+    when 2
+      p "=================================="
+      p "Add a Bounty"
+      p "=================================="
+      p "Name: "
+      name = gets.chomp
+      p "Species: "
+      species = gets.chomp
+      p "Bounty Amount: "
+      bounty = gets.chomp.to_i
+
+      new_bounty = Bounties.new({
+        'name' => name,
+        'species' => species,
+        'bounty' => bounty
+      })
+      new_bounty.save()
+    when 3
+      p "=================================="
+      p "Update a Bounty"
+      p "=================================="
+      # ask for then display entry to be edited
+      p "Enter an ID to alter: "
+      id = gets.chomp.to_i
+      bounty_list = Bounties.view_one(id)
+      # binding.pry
+      bounty_list.each do |bounty|
+        p "Bounty #{bounty.id}: #{bounty.name} (#{bounty.species}) - Ω#{bounty.bounty}"
+
+        name = bounty.name
+        species = bounty.species
+        bounty = bounty.bounty.to_i
+      end
+      # ask which attribute to change
+      p "name - Change Name"
+      p "species - Change Species"
+      p "bounty - Change Bounty Amount"
+      edit_input = gets.chomp
+      #update attribute
+      case edit_input
+        when 'name'
+          p "Input Name: "
+          name = gets.chomp
+        when 'species'
+          p "Input Species: "
+          species = gets.chomp
+        when 'bounty'
+          p "Input Bounty: "
+          bounty = gets.chomp.to_i
+        else
+          p "Incorrect Input"
+      end
+      # binding.pry
+      Bounties.update(name,species,bounty,id)
+    when 4
+      p "=================================="
+      p "Delete a Bounty"
+      p "=================================="
+      p "Type the ID of the Bounty to delete, or 'all' to remove all records."
+      delete_input = gets.chomp
+      if delete_input == 'all'
+        Bounties.delete_all()
+      else
+        delete_input.to_i
+        Bounties.delete(delete_input)
+      end
+    else
+      p "Goodbye"
+      quitloop = true
+
+  end
+end
--- models/space_bounties.rb
+++ models/space_bounties.rb
@@ -1,29 +1,103 @@
 require 'pg'
 
 class Bounties
-  attr_reader :name,:species,:bounty
+  attr_accessor :name,:species,:bounty
+  attr_reader :id
 
   def initialize(options)
-    @name = name
-    @species = species
-    @bounty = bounty
+    @name = options['name']
+    @species = options['species']
+    @bounty = options['bounty']
+    @id = options['id'].to_i
   end
 
   def save()
+    db = PG.connect({
+        dbname: 'space_bounties',
+        host: 'localhost'
+    })
+    sql = "
+      INSERT INTO space_bounties (
+        name,species,bounty
+      )
+      VALUES
+      ($1,$2,$3)
+      RETURNING id
+    ;"
+    # Run Query
+    db.prepare('save',sql)
+    result = db.exec_prepared('save',[@name,@species,@bounty])
 
+    db.close()
+
+    id = result[0]['id'].to_i
+    @id = id
+  end
+
+  def self.all()
+    db = PG.connect({dbname:'space_bounties',host:'localhost'})
+
+    sql = "SELECT * FROM space_bounties"
+    db.prepare('all',sql)
+    bounty_hashes = db.exec_prepared('all')
+    db.close()
+
+    bounty_objects = bounty_hashes.map do |bounty_hash|
+      Bounties.new(bounty_hash)
+    end
+    return bounty_objects
+  end
+
+  def self.view_one(input_id)
+    db = PG.connect({dbname:'space_bounties',host:'localhost'})
+
+    sql = "SELECT * FROM space_bounties WHERE id = $1"
+    db.prepare('all',sql)
+    bounty_hashes = db.exec_prepared('all',[input_id])
+    db.close()
+
+    bounty_objects = bounty_hashes.map do |bounty_hash|
+      Bounties.new(bounty_hash)
+    end
+    return bounty_objects
   end
 
   def self.delete_all()
+    db = PG.connect({dbname:'space_bounties',host:'localhost'})
+
+    sql = "DELETE FROM space_bounties"
+    db.prepare('all',sql)
+    db.exec_prepared('all')
   end
 
-  def self.delete()
+  def self.delete(bounty_id)
+    db = PG.connect({
+        dbname: 'space_bounties',
+        host: 'localhost'
+    })
 
+    sql = "
+      DELETE FROM space_bounties WHERE id = #{bounty_id};
+    "
+    # Run Query
+    db.exec(sql)
+    db.close()
   end
 
-  def update()
+  def self.update()
+    db = PG.connect({dbname: 'space_bounties',host:'localhost'})
+    binding.pry
+    sql = "
+      UPDATE space_bounties
+      SET (name,species,bounty)
+      = ($1,$2,$3)
+      WHERE id = $4
+      ;"
 
+      values = [@name,@species,@bounty,@id]
+
+    db.prepare('update',sql)
+    db.exec_prepared('update',values)
+    db.close()
   end
-
-  
-
 end
